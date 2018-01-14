b144af8f894be1ea8da1910708b7f6ea18e18b24
Andy Duncan <andy@adjectivecolournoun.com>
2018-01-14 22:38:39 +0000

    Some friendly grouping

--- functions/pugna/handler.js
+++ functions/pugna/handler.js
@@ -77,18 +77,21 @@ const surroundingPoints = (positions, point) => {
 const distanceToPoint = (a, b) => Math.sqrt(Math.pow(a.coordinate.x - b.coordinate.x, 2) +
     Math.pow(a.coordinate.y - b.coordinate.y, 2));
 
-const findClosestOpponent = (positions) => (target) => positions
-    .filter((p) => p.playerName !== target.playerName)
+const findClosest = (positions, targetPlayer) => (target) => positions
+    .filter((p) => p.playerName === targetPlayer)
     .sort((a, b) =>
         distanceToPoint(target, a) - distanceToPoint(target, b)
     )[0];
+
+// const findClosestOpponent = (positions, targetPlayer) => (target) => positions
+//     .filter((p) => p.playerName === targetPlayer)[0];
 
 const checkSafeMove = (positions) => (playerName) => (p) => {
     const captureSurrounds = surroundingPoints(positions, p.point);
     return !captureSurrounds.some(c => c.point.playerName && (c.point.playerName !== playerName));
 };
 
-const determineNextMove = (positions, target, boardSize) => {
+const determineNextMove = (positions, target, targetPlayer, boardSize) => {
     if (!positions.some((p) => p.playerName !== target.playerName)) {
         debug('We already won!');
         return 'STAY';
@@ -110,30 +113,45 @@ const determineNextMove = (positions, target, boardSize) => {
     const openPoints = eligiblePoints
         .filter(p => !p.point.playerName);
 
-    const closest = findClosestOpponent(positions);
+    const findClosestOpponent = findClosest(positions, targetPlayer);
+
+    const findClosestFriend = findClosest(positions, target.playerName);
+
+    const backup = eligiblePoints.filter(p => p.point.playerName === target.playerName).length;
 
     const safeOpenPoints = openPoints
         .filter(safe)
         .map(p => {
-            const closestOpponent = closest(target, positions);
-            return [p, distanceToPoint(closestOpponent, p.point)]
+            const closestOpponent = findClosestOpponent(target);
+            const closestFriend = findClosestFriend(target);
+            debug(JSON.stringify(closestOpponent));
+            return [p, backup > 1 ? distanceToPoint(closestOpponent, p.point): distanceToPoint(closestFriend, p.point)];
+            // return [p, distanceToPoint(closestOpponent, p.point)];
         })
         .sort((a, b) => a[1] - b[1])
         .map(pd => pd[0]);
 
-    if (safeCaptures.length) {
-        debug('Safe captures: ' + JSON.stringify(safeCaptures));
-        return safeCaptures[0].move;
-    }
-
-    if (safeOpenPoints.length) {
-        debug('Safe open points: ' + JSON.stringify(safeOpenPoints));
-        return safeOpenPoints[0].move;
-    }
-
     if (possibleCaptures.length) {
         debug('Unsafe captures: ' + JSON.stringify(possibleCaptures));
         return possibleCaptures[0].move;
+    }
+
+    if (backup > 1) {
+        if (openPoints.length) {
+            debug('Unsafe open points: ' + JSON.stringify(openPoints));
+            return openPoints[0].move;
+        }
+    } else {
+
+        // if (safeCaptures.length) {
+        //     debug('Safe captures: ' + JSON.stringify(safeCaptures));
+        //     return safeCaptures[0].move;
+        // }
+        //
+        if (safeOpenPoints.length) {
+            debug('Safe open points: ' + JSON.stringify(safeOpenPoints));
+            return safeOpenPoints[0].move;
+        }
     }
 
     debug('Doing nothing');
@@ -152,13 +170,21 @@ module.exports.nextMove = (event, context, callback) => {
 
     const boardSize = game.boardState.boardSize;
 
+    const targetPlayer = game.boardState.players
+        .filter(p => p !== target.playerName
+            && positions.some(q => q.playerName === p))
+        .map(p => [p, positions.filter(q => q.playerName === p).length])
+        .sort((a, b) => a[1] - b[1])[0][0];
+
     debug('Point to move: ' + JSON.stringify(target));
 
     debug('Board state: ' + JSON.stringify(positions));
 
+    debug('Targeting player: ' + targetPlayer);
+
     const response = {
         statusCode: 200,
-        body: determineNextMove(positions, target, boardSize)
+        body: determineNextMove(positions, target, targetPlayer, boardSize)
     };
 
     callback(null, response);
